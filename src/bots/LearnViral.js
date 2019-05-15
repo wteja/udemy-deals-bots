5ea632a96de99830b253c64130d6c6357b9da9d0
Weerayut Teja <teja.weerayut@gmail.com>
2019-05-15 17:31:34 +0700

    Change bot flow

--- src/bots/LearnViral.js
+++ src/bots/LearnViral.js
@@ -1,103 +1,103 @@
-const cheerio = require('cheerio');
-const htmlToText = require('html-to-text');
-const timer = require('../utilities/timer');
-const courseHelper = require('../utilities/course');
+const cheerio = require('cheerio')
+const htmlToText = require('html-to-text')
+const timer = require('../utilities/timer')
+const courseHelper = require('../utilities/course')
 
 module.exports = class LearnViral {
 
     constructor(repo, browser) {
-        this.repo = repo;
-        this.browser = browser;
-        this.baseUrl = `https://udemycoupon.learnviral.com/coupon-category/free100-discount/`;
-        this.page = 1;
-        this.working = true;
+        this.repo = repo
+        this.browser = browser
+        this.baseUrl = `https://udemycoupon.learnviral.com/coupon-category/free100-discount/`
+        this.page = 1
+        this.working = true
     }
 
     start() {
         return new Promise(async (resolve, reject) => {
-            while (this.working) {
-                const pageUrl = this.page > 1 ? this.baseUrl + `page/${this.page}/` : this.baseUrl;
-                const pageInfo = await this.getCourseListPageInfo(pageUrl);
-
-                await timer.randomDelay();
-
-                const courses = await this.getAllCoursesByLinks(pageInfo.links);
-                courses.forEach(async course => {
-                    const isExists = await this.repo.isUrlExists(course.url);
-                    if (!isExists) {
-                        await this.repo.add(course);
+            try {
+                let pageLinks = []
+                while (this.working) {
+                    const pageUrl = this.page > 1 ? this.baseUrl + `page/${this.page}/` : this.baseUrl
+                    const pageInfo = await this.getCourseListPageInfo(pageUrl)
+                    pageLinks = pageLinks.concat(pageInfo.links)
+    
+                    await timer.randomDelay(5, 10)
+    
+                    if (pageInfo.hasNextPage) {
+                        this.page++
+                        await timer.randomDelay(5, 10)
+                    } else {
+                        this.page = 1
+    
+                        pageLinks.reverse() // Reverse to save old data first.
+                        
+                        for (let i = 0, n = pageLinks.length; i < n; i++) {
+                            const course = await this.getCourseByLink(pageLinks[i])
+                            const isExists = await this.repo.isUrlExists(course.url)
+                            if (!isExists) {
+                                await this.repo.add(course)
+                            }
+                            await timer.randomDelay(5, 10)
+                        }
+    
+                        pageLinks = [] // Clear all links.
+    
+                        await timer.delay(60 * 60 * 6)
                     }
-                })
-
-                if (pageInfo.hasNextPage) {
-                    this.page++;
-                    await timer.randomDelay(5, 10);
-                } else {
-                    this.page = 1;
-                    await timer.randomDelay(60);
                 }
+    
+                resolve()
+            } catch (err) {
+                reject(err)
             }
-
-            resolve();
-        });
+        })
     }
 
     stop() {
         return new Promise((resolve, reject) => {
-            this.working = false;
-            resolve();
-        });
+            this.working = false
+            resolve()
+        })
     }
 
     getCoursesLinks() {
         return new Promise((resolve, reject) => {
-            resolve([]);
-        });
+            resolve([])
+        })
     }
 
     getCourseListPageInfo(url) {
         return new Promise(async (resolve, reject) => {
-            const html = await this.browser.getHtml(url);
-            const $ = cheerio.load(html);
-            const $content = $('#content');
-            const $activeContentBox = $content.find('.content-box:nth-child(2)');
-            const $couponCodeLinks = $activeContentBox.find('.entry-title a');
-            const links = $couponCodeLinks.toArray().map(a => a.attribs.href);
-            const hasNextPage = $activeContentBox.find('.paging .next').length > 0;
+            const html = await this.browser.getHtml(url)
+            const $ = cheerio.load(html)
+            const $content = $('#content')
+            const $activeContentBox = $content.find('.content-box:nth-child(2)')
+            const $couponCodeLinks = $activeContentBox.find('.entry-title a')
+            const links = $couponCodeLinks.toArray().map(a => a.attribs.href)
+            const hasNextPage = $activeContentBox.find('.paging .next').length > 0
             const returns = {
                 links,
                 hasNextPage
-            };
-            resolve(returns);
-        });
-    }
-
-    getAllCoursesByLinks(links) {
-        return new Promise(async (resolve, reject) => {
-            const promises = links.map(async link => {
-                await timer.randomDelay();
-                return this.getCourseByLink(link);
-            });
-            Promise.all(promises)
-                .then(result => {
-                    resolve(result);
-                })
-                .catch(reject);
-        });
+            }
+            resolve(returns)
+        })
     }
 
     getCourseByLink(link) {
+        console.log('Save link: ' + link)
         return new Promise(async (resolve, reject) => {
-            const html = await this.browser.getHtml(link);
-            const $ = cheerio.load(html);
-            const title = $('.blog .entry-title a').text().replace('[Free] ', '');
-            const $textBox = $('.blog .text-box');
-            $textBox.find('h2').remove();
-            const description = htmlToText.fromString($textBox.html());
-            const couponUrl = $('.coupon-code-link').attr('href');
-            const course = courseHelper.create(title, description, couponUrl);
-            resolve(course);
-        });
+            const html = await this.browser.getHtml(link)
+            const $ = cheerio.load(html)
+            const title = $('.blog .entry-title a').text().replace('[Free] ', '')
+            const $textBox = $('.blog .text-box')
+            $textBox.find('h2').remove()
+            const description = htmlToText.fromString($textBox.html())
+            const thumbnailUrl = $('.store-image img').attr('src')
+            const couponUrl = $('.coupon-code-link').attr('href')
+            const course = courseHelper.create(title, description, couponUrl, thumbnailUrl)
+            resolve(course)
+        })
     }
 
 }
--- src/utilities/course.js
+++ src/utilities/course.js
@@ -1,9 +1,10 @@
-const create = (title, description, url) => {
+const create = (title, description, url, thumbnailUrl) => {
     const now = new Date();
     return {
         title,
         description,
         url,
+        thumbnailUrl,
         successCount: 1,
         created_at: now,
         updated_at: now
